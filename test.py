073e1dba45d7f0b8fa3ffbd3c81706576f145b93
rmehrizi <45501183+rmehrizi@users.noreply.github.com>
2020-02-11 15:23:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ import plots
 marker_data=pd.read_csv('marker_data.txt',sep='\t')
 fp_data=pd.read_csv('fp_data.txt',sep='\t')
 body_mass = 68
+height = 1.6
 
 # time distance moduale
 events = td.event_detection(fp_data, body_mass)
@@ -33,4 +34,6 @@ m = id.mass(body_mass, 1)
 com = id.center_of_mass(marker_data, 1)
 cm_dd = id.derivative(com, 0.01, 2)
 f = id.force(fp_data, m, cm_dd)
-plots.forces_plot(f)
+plots.forces_plot(f, body_mass)
+m = id.moment(fp_data, marker_data, m, com, cm_dd, f)
+plots.moments_plot(f, body_mass, height)
