8438780f1d034c82f0a40eb83196e05f926659b0
rmehrizi <45501183+rmehrizi@users.noreply.github.com>
2020-03-05 09:42:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,32 +4,31 @@
 import pandas as pd
 import inverse_kinematics as ik
 import time_distance as td
-import numpy as np
-import math
 import inverse_dynamics as id
 import plots 
 
 # reading data
-marker_data=pd.read_csv('marker_data.txt',sep='\t')
-fp_data=pd.read_csv('fp_data.txt',sep='\t')
+marker_data = pd.read_csv('...\marker_data.txt', sep='\t')
+fp_data = pd.read_csv('...\fp_data.txt', sep='\t')
 body_mass = 68
 height = 1.6
 
-# time distance moduale
+# time distance module
 events = td.event_detection(fp_data, body_mass)
-plots.events_plot(fp_data, 'all', 100)
-cad = cadence(events)
+plots.events_plot(fp_data, events, 'all', 100)
+cad = td.cadence(events)
 print('Cadence average is {0:.2f} steps per minute'.format(cad, 2))
-ratio = stance_ratio(events)
-print('Swing phase consists {0:0.2f} and {1:.2f} percent of gait cycle for left and right leg, respectively'.format(ratio[0], ratio[1]))
+ratio = td.stance_ratio(events)
+print('Swing phase consists {0:0.2f} and {1:.2f} percent of gait cycle for left and right leg, respectively'
+      .format(ratio[0], ratio[1]))
 
-# inverse kinematics moduale
+# inverse kinematics module
 segs = ik.segments(marker_data)
 l = ik.length(segs)
 angs = ik.angles(segs)
 plots.angles_plot(angs)
 
-# inverse dynamics moduale
+# inverse dynamics module
 m = id.mass(body_mass, 1)
 com = id.center_of_mass(marker_data, 1)
 cm_dd = id.derivative(com, 0.01, 2)
